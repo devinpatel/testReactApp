eb5419ea91dd4009117ce7f460bbfc30c1519e37
Devin Patel <devin.patel@anautics.com>
2020-10-30 17:22:08 -0400

    added unit and integration tests

--- src/App.js
+++ src/App.js
@@ -27,3 +27,7 @@ export default App;
 export const add = (x, y) => {
   return x + y;
 };
+
+export const total = (shipping, subtotal) => {
+  return '$' + add(shipping, subtotal);
+};
--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,13 @@
 //   expect(linkElement).toBeInTheDocument();
 // });
 
-import { add } from './App';
+import { add, total } from './App';
 
 test('add', () => {
   const value = add(1, 2);
   expect(value).toBe(3);
+});
+
+test('total', () => {
+  expect(total(5, 20)).toBe('$25');
 });
